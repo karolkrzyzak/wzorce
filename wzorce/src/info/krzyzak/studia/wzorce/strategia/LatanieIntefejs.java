39a9382320e4170ed1783890c249b4c86e406073
Karol Krzyzak <karol.krzyzak@gmail.com>
2011-12-09 21:05:02 +0100

    Pierwszy comit na git hub moje wzroce

---
+++ wzorce/src/info/krzyzak/studia/wzorce/strategia/LatanieIntefejs.java
@@ -0,0 +1,14 @@
+package info.krzyzak.studia.wzorce.strategia;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: karolkrzyzak
+ * Date: 12/9/11
+ * Time: 9:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface LatanieIntefejs {
+
+    public void lec();
+
+}
