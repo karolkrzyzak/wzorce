7c8038c535c16db5f5c8231cd70f8b6aecaa119a
Karol Krzyzak <karol.krzyzak@gmail.com>
2012-01-31 23:45:57 +0100

    projekt webSerwices

---
+++ jaxwsSpring/jaxws-spring/src/main/java/info/kkrzyzak/bo/MyBo.java
@@ -0,0 +1,18 @@
+package info.kkrzyzak.bo;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: karolkrzyzak
+ * Date: 1/31/12
+ * Time: 8:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MyBo {
+
+    String getName();
+    
+    String getLastName();
+    
+    int getAge();
+
+}
