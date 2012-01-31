cbcd7a04d4543a404de4630fe057035313b9063d
Karol Krzyzak <karol.krzyzak@gmail.com>
2012-01-31 23:14:22 +0100

    jaxwsZeSpringiem

---
+++ jaxwsSpring/jaxws-spring/src/test/java/info/kkrzyzak/bo/MyWSTest.java
@@ -0,0 +1,37 @@
+package info.kkrzyzak.bo;
+
+/*import info.kkrzyzak.generatedForTests.MyWS;
+import info.kkrzyzak.generatedForTests.MyWS_Service;*/
+
+
+/**
+ * TODO i will be back :D
+ * Created by IntelliJ IDEA.
+ * User: karolkrzyzak
+ * Date: 1/31/12
+ * Time: 10:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyWSTest{
+   /* protected static URL wsdlURL;
+    protected static QName serviceName;
+
+    static {
+        serviceName = new QName("http://ws.kkrzyzak.info/", "MyWSService");
+        String address = "http://localhost:8080/jsWeb/jaxwsSpring";
+        try {
+            wsdlURL = new URL(address + "?wsdl");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+
+    @Test
+    public void doubleItWorksWithPositiveNumbers() throws MalformedURLException {
+      *//*  MyWS_Service myWS_service = new MyWS_Service(wsdlURL,serviceName);
+        MyWS myWS = myWS_service.getMyWS();
+        String name = myWS.getName();
+        assertEquals("Name as i expexcted id ", "Karol", name);*//*
+    }*/
+}
